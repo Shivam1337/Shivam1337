c5976e9d8122d2c19a41cc7ce59b584b1c44072c
Shivam <shivamshinde883@gmail.com>
2022-01-31 22:18:32 +0530

    Added HTML SPA

--- shivamshinde.com/src/App.js
+++ shivamshinde.com/src/App.js
@@ -1,10 +1,8 @@
 import './App.css';
-import resume from './resume.jpg';
 
 function App() {
   return (
     <div className="App">
-      <img className="resume" src={resume} alt="resume"/>
     </div>
   );
 }
